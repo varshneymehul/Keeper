d9b59e97d4bd74653fe3008535040e8f4fafeb22
Mehul Varshney <varshneymehul5@gmail.com>
2021-04-27 14:00:01 +0530

    Added extensions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
-import AddNote from "./AddNote";
-
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import AddNote from "./AddNote.jsx";
 
 function App() {
   const [notes, setNotes] = useState([]);

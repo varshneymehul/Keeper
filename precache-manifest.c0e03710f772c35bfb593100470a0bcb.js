e5c6f4af6f557d7c6f87c2cd3ea163ffc610b505
Mehul Varshney <varshneymehul5@gmail.com>
2021-06-23 09:04:16 +0530

    Updates

---
+++ precache-manifest.c0e03710f772c35bfb593100470a0bcb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5fa0ff9d6e781f2ad732369a72be863",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "a2a62356bc11d5c9034d",
+    "url": "/Keeper/static/js/2.51044d01.chunk.js"
+  },
+  {
+    "revision": "57b0775e7cbfb1707e05",
+    "url": "/Keeper/static/js/main.1afb1126.chunk.js"
+  },
+  {
+    "revision": "7e94f4bb0c0b87bffef0",
+    "url": "/Keeper/static/js/runtime-main.4f47787b.js"
+  }
+]);
